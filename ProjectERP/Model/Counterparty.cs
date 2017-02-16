e4366f9e5ba7a54a70eeb668b0a3d278feea3c44
Krzysztof Baranowski <baranowski@statica.pl>
2017-02-16 16:24:28 +0100

    pierwszy commit

---
+++ ProjectERP/Model/Counterparty.cs
@@ -0,0 +1,41 @@
+﻿using GalaSoft.MvvmLight;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectERP.Model
+{
+    public class Counterparty: ObservableObject
+    {
+        private int id;
+        private string name;
+        private int age;
+        private decimal salary;
+
+        public const string MyPropertyPropertyName = "MyProperty";
+
+        private bool _myProperty = false;
+
+        public bool MyProperty
+        {
+            get
+            {
+                return _myProperty;
+            }
+            
+            set
+            {
+                if (_myProperty == value)
+                {
+                    return;
+                }
+
+                _myProperty = value;
+                RaisePropertyChanged(MyPropertyPropertyName);
+            }
+        }
+    }
+
+}
