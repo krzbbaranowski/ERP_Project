06f351d39c3bea0d73bb48d5c853695dce877bba
Krzysztof Baranowski <baranowski@statica.pl>
2017-02-16 17:49:26 +0100

    .

---
+++ ProjectERP/Views/UiControls/Table.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows.Controls;
+
+namespace ProjectERP.Views.UiControls
+
+{
+    /// <summary>
+    /// Interaction logic for Table.xaml
+    /// </summary>
+    public partial class Table : UserControl
+    {
+        public ObservableCollection<object> Items { get; set; } = new ObservableCollection<object>();
+
+        public Table()
+        {
+            InitializeComponent();
+        }
+    }
+}
